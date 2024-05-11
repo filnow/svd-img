c7640650bc33ba54fb5ceb050802f102b10f35c8
filnow <filnow3@st.amu.edu.pl>
2024-05-11 14:20:29 +0200

    fix frame refresh, hide button, slider, fix compression rate

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ from scipy.ndimage import gaussian_filter
 def compression_rate(it: int, Ms: int, Ns: int) -> float: 
     return 1-it*(Ms+Ns+1)/(Ms*Ns)*1.0
 
+def rmse(img_array: np.ndarray, compressed_img_array: np.ndarray) -> float:
+    return np.sqrt(np.mean((img_array-compressed_img_array)**2))
+
 def display_images(img_array: np.ndarray, 
                    compressed_img_array: np.ndarray) -> None:
     fig, ax = plt.subplots(1, 2, figsize=(12, 6))
@@ -54,34 +57,36 @@ if __name__ == "__main__":
                 }
         
     st.title("SVD Image Compression")
-    stop_button = st.button("Stop video", on_click=destroy)
     
+    button_placeholder = st.empty()
     slider_placeholder = st.empty() 
-    frame_placeholder = st.empty() 
-
-    n_components = slider_placeholder.slider("Number of components:", 
-                                min_value=1, max_value=100, value=5, key="slider") 
-
+    
     # Load the webcam
     if "load" not in st.session_state or not st.session_state.initialized:
-        cap = cv2.VideoCapture(0)    
-        st.session_state["load"] = cap
+        st.session_state["load"] = cv2.VideoCapture(0)
+        st.session_state["frame"] = st.empty()   
+        st.session_state["slider"] = st.empty()
+        st.session_state["button"] = st.empty()
         st.session_state.initialized = True
         
     # Display the webcam with SVD compression
     if "init" not in st.session_state:
-        while st.session_state["load"].isOpened() and not stop_button:
+        st.session_state["button"] = button_placeholder.button("Stop video", on_click=destroy)
+        st.session_state["slider"] = slider_placeholder.slider("Number of components:", 
+                                min_value=1, max_value=100, value=5) 
+        
+        while st.session_state["load"].isOpened() and not st.session_state["button"]:
             ret, frame = st.session_state["load"].read()
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY).astype(np.float32)
-
+            
             u, s, v = np.linalg.svd(gray, full_matrices=False)
 
-            compressed_img_array = u[:, :n_components] @ np.diag(s[:n_components]) @ v[:n_components, :]
+            compressed_img_array = u[:, :st.session_state["slider"]] @ np.diag(s[:st.session_state["slider"]]) @ v[:st.session_state["slider"], :]
             
-            frame_placeholder.image((compressed_img_array/255).astype(np.float32), 
+            st.session_state["frame"].image((compressed_img_array/255).astype(np.float32), 
                                     channels="GRAY", clamp=True, use_column_width=True)
 
-            if cv2.waitKey(1) & 0xFF == ord('q') or stop_button:
+            if cv2.waitKey(1) & 0xFF == ord('q') or st.session_state["button"]:
                 st.session_state["load"].release()
                 cv2.destroyAllWindows()
                 break    
@@ -92,13 +97,16 @@ if __name__ == "__main__":
 
     # Display the image with SVD compression
     if uploaded_file is not None:
+        st.session_state["button"] = st.empty()
+        st.session_state["slider"] = st.empty()
+
         img_array = np.array(cv2.imread("./images/" + uploaded_file.name)).astype(np.float32)       
         
         gray_checkbox = st.checkbox("Gray scale", False)
 
         # Number of components for the SVD
         n = st.slider("Number of components:", min_value=1, 
-                        max_value=int(min(img_array.shape[:2])/10), value=10, key="slider_img")
+                        max_value=max(img_array.shape[:2])//3, value=10, key="slider_img")
         
         # Function to scall the singular values
         option = st.selectbox("Function to scall singular values:", (scall_func.keys()), key="option")
@@ -107,6 +115,7 @@ if __name__ == "__main__":
             # Gray scale image
             img_array = cv2.cvtColor(img_array, cv2.COLOR_BGR2GRAY).astype(np.float32)
             compressed_img_array = svd(img_array, n, scall_func, option)
+            print(compressed_img_array.shape)
         else:
             # RGB image
             img_array = cv2.cvtColor(img_array/255, cv2.COLOR_BGR2RGB)
@@ -116,6 +125,6 @@ if __name__ == "__main__":
             for i, channel in enumerate([red, green, blue]):
                 compressed_img_array[:, :, i] = svd(channel, n, scall_func, option)
             
-        st.subheader(f"Compression rate: {(100*compression_rate(5*n, img_array.shape[0], img_array.shape[1])):.2f}%")    
+        st.subheader(f"Compression rate: {(100*compression_rate(n, img_array.shape[0], img_array.shape[0])):.2f}%")    
         
         display_images(img_array, compressed_img_array)
